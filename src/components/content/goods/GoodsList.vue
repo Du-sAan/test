aefe08bc594efb5fe9dfbeea0c9d724f700b9e26
mhm <hm_m0513@qq.com>
2020-12-22 18:07:01 +0800

    商品展示

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="goods-list">
+    <goods-list-item
+    v-for="(item,id) of goods"
+    :key="id"
+    :goodsItem="item" />
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "./GoodsListItem.vue"
+export default {
+  name : "GoodsList",
+  props : {
+    goods : {
+      type : Array,
+      default (){
+        return []
+      }
+    }
+  },
+  components: {
+    GoodsListItem
+  }
+}
+</script>
+
+<style>
+  .goods-list{
+    display: flex;
+    /* 按照侧轴换行 */
+    flex-wrap: wrap;
+    /* 空白环绕弹性元素周围 */
+    justify-content: space-around;
+    padding: 2px;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,47 +7,9 @@
     </NavBar>
     <HomeSwiper :banners="banners" class="home-swiper"></HomeSwiper>
     <HomeRecom-view :recommend="recommend"></HomeRecom-view>
-    <home-fearture/>
+    <home-fearture></home-fearture>
     <tab-control :titles="['流行','新款','精选']" class="tab-control" />
-    <ul>
-      <li>1</li>
-      <li>3</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>0</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-      <li>3</li>
-    </ul>
+    <goods-list :goods="goods['pop'].list"></goods-list>
   </div>
 
 </template>
@@ -55,8 +17,10 @@
 <script>
 import NavBar from "components/common/navbar/NavBar"
 import TabControl from "components/content/TabControl/TabControl"
+import GoodsList from "components/content/goods/GoodsList.vue"
 
-import {getHomeMultidata} from "network/home_request.js"
+import {getHomeMultidata,getHomeGoods} from "network/home_request.js"
+
 
 import HomeSwiper from "./childComps/Swiper.vue"
 import HomeRecomView from "./childComps/RecomView.vue"
@@ -66,6 +30,7 @@ export default {
   components : {
     NavBar,
     TabControl,
+    GoodsList,
     HomeSwiper,
     HomeRecomView,
     HomeFearture,
@@ -73,12 +38,26 @@ export default {
   data () {
     return {
       banners : [],
-      recommend : []
+      recommend : [],
+      goods : {
+        'pop' : {page : 0, list : []},
+        'new' : {page : 0, list : []},
+        'sell' : {page : 0, list : []},
+      }
     }
   },
   // 当组件被创建时，请求网络请求
   created () {
-    getHomeMultidata().then( (res) => {
+    // 请求轮播图和推荐的数据
+    this.getHomeMultidata()
+    // 请求主页商品的数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods :{
+    getHomeMultidata(){
+      getHomeMultidata().then( (res) => {
       // 取出网络请求返回的data,并存放在home的data中
       // 这里因为是箭头函数，所以this是created的this,
       // 而created的this指向实现created的组件
@@ -87,8 +66,17 @@ export default {
       this.recommend = res.data.recommend.list
       // 拦截器 this.banner = red.data.data.recommend.list
       // console.log(res.data)
-    })
-  },
+      })
+    },
+    getHomeGoods(type){
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then((res)=>{
+        // 将请求到的数据，添加到对应type的list中,在将相应的页码+1
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      })
+    }
+  }
 }
 </script>
 
@@ -114,6 +102,7 @@ export default {
   }
   .tab-control{
     position: sticky;
-    top: 44px;
+    top: 43px;
+    z-index: 9;
   }
 </style>
