2889678265060cfcca0322892d915f0df22098f0
mhm <hm_m0513@qq.com>
2021-01-16 19:58:12 +0800

    响应式布局

--- 响应式/test.js
+++ 响应式/test.js
@@ -1,7 +1,14 @@
 const burger = document.querySelector(".nav-burger");
 let menu = document.querySelector(".nav-menu");
+let menuItems = document.querySelectorAll(".nav-menu li");
 burger.addEventListener("click", ()=> {
-  burger.classList.toggle("active")
-  menu.classList.toggle("open")
+  burger.classList.toggle("active");
+  menu.classList.toggle("open");
+  menuItems.forEach( (value, index) => {
+    if(value.style.animation){
+      value.style.animation = ""
+    }else {
+      value.style.animation = `0.3s ease-in menuItems forwards ${index * 0.1 + 1}s`
+    }
+  });
 },false);
-console.log()
